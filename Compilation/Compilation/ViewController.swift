64775c5c04231b7f3e487aab61ed879a0f751f3b
Michael Reynolds <reynoldsmichael945@gmail.com>
2017-05-15 11:53:29 -0400

    Saving initial progress

--- Compilation/Compilation/ViewController.swift
+++ Compilation/Compilation/ViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    var currentCount = Int(arc4random()%100)
+    
+    @IBOutlet weak var thisTextField:UITextField!
+    @IBOutlet weak var thisHereSlider:UISlider!
+    @IBOutlet weak var mrStepper:UIStepper!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
